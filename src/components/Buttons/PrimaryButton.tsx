b495c298cc33bf475f16f727f47c280f215419e6
Neja Dolinar <dolinar.neja@gmail.com>
2019-12-11 12:44:07 +0100

    Adjust PrimaryButton to handle both onClick and onMouseDown.

--- src/components/Buttons/PrimaryButton.tsx
+++ src/components/Buttons/PrimaryButton.tsx
@@ -8,16 +8,35 @@ type ButtonType = 'primary' | 'secondary';
 
 interface Props {
   onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  onMouseDown?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
   children: string;
   isSubmit?: boolean;
   buttonType?: ButtonType;
+  mousedownInteraction?: boolean;
 }
 
-export const PrimaryButton: React.FC<Props> = ({ buttonType, onClick, children, isSubmit }) => {
+export const PrimaryButton: React.FC<Props> = ({
+  buttonType,
+  onClick,
+  children,
+  isSubmit,
+  mousedownInteraction,
+}) => {
+  const stopEvent = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    onClick(e);
+  };
+
   const classNames = cn('', buttonType || '');
 
   return (
-    <button className={classNames} onClick={onClick} type={isSubmit ? 'submit' : 'button'}>
+    <button
+      {...{
+        className: classNames,
+        type: isSubmit ? 'submit' : 'button',
+        ...(mousedownInteraction ? { onMouseDown: onClick } : { onClick }),
+      }}
+    >
       {children}
     </button>
   );
