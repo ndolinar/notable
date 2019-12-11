b092543519c21f8bae3f96eb96abed71a1adf9e8
Neja Dolinar <dolinar.neja@gmail.com>
2019-12-11 12:42:02 +0100

    Make payload optional in createAction.

--- src/store/utils/index.ts
+++ src/store/utils/index.ts
@@ -9,7 +9,7 @@ interface ActionWithPayload<T, P> extends Action<T> {
 }
 
 // const functionName = <generics>(param: type): returnType => {}
-export const createAction = <T, P>(type: T, payload: P): Action<T> | ActionWithPayload<T, P> => {
+export const createAction = <T, P>(type: T, payload?: P): Action<T> | ActionWithPayload<T, P> => {
   if (payload) {
     return { type, payload };
   } else {
